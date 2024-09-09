3da1e45983a2a1238f23eef5c925bdcc8b73f97c
EmirAycicek <132485519+EmirAycicek@users.noreply.github.com>
2024-09-09 12:54:15 +0300

    deneme

--- app/(routes)/_components/Menu/UserMenu.tsx
+++ app/(routes)/_components/Menu/UserMenu.tsx
@@ -42,7 +42,7 @@ const UserMenu = () => {
             <UserCircleIcon className="h-6 w-6" />
           </DropdownMenuTrigger>
           <DropdownMenuContent>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuLabel>My Account cartcurt</DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuItem>Profile</DropdownMenuItem>
             <DropdownMenuItem onClick={onSignOut}>Logout</DropdownMenuItem>
